244f17c073d73019030a40183ec5510b70606fb6
Igor <114850837+IgorZhura@users.noreply.github.com>
2023-06-26 17:21:08 +0300

    Add files via upload

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 let images = [{
-    url: "./images/slider-1.jpg",
+    url: "./slider-1.jpg",
   }, {
-    url: "./images/slider-2.jpg",
+    url: "./slider-2.jpg",
   }, {
-    url: "./images/slider-3.jpg",
+    url: "./slider-3.jpg",
 }];
 
 function sliderInit(images, options) {
@@ -19,10 +19,13 @@ function sliderInit(images, options) {
   let sliderWrapper = document.querySelector(".slider");
   let sliderImages = document.querySelector(".slider__images");
   let sliderArrows = document.querySelector(".slider__arrows");
+  let dotsWrapper = document.querySelector(".slider__dots");
+  let navWraper = document.querySelector(".slider__nav__li__item");
   let intervalId;
   
   initImages();
   initArrows();
+  initNav();
   
   if (options.dots) {
     initDots();
@@ -70,17 +73,13 @@ function sliderInit(images, options) {
     sliderImages.querySelector(`.n${num}`).classList.add("active");
     
     if (options.dots) {
-      let dotsWrapper = document.querySelector(".slider__dots");
       dotsWrapper.querySelector(".active").classList.remove("active");
       dotsWrapper.querySelector(`.n${num}`).classList.add("active");
     }
   }
   
-  function initDots() {
-    let dotsWrapper = document.createElement("div");
-    dotsWrapper.className = "slider__dots";
-    
-    images.forEach((image, index) => {
+  function initDots() {    
+      images.forEach((image, index) => {
       let dot = document.createElement("div");
       dot.className = `slider__dots-item n${index} ${index === 0? "active" : ""}`
       dot.dataset.index = index;
@@ -92,7 +91,21 @@ function sliderInit(images, options) {
       });
       dotsWrapper.appendChild(dot);
     });
-    sliderWrapper.appendChild(dotsWrapper);
+  }
+
+  function initNav() {
+    images.forEach((image, index) => {
+      let nav = document.createElement("li");
+      nav.className = `slider__nav__li__item n${index} ${index === 0? "active" : ""}`
+      nav.dataset.index = index;
+      nav.addEventListener("click", function() {
+        moveSlider(nav.dataset.index);
+        if (options.autoplay) {
+          clearInterval(intervalId);
+        }
+      });
+      navWrapper.appendChild(nav);
+    });
   }
   
   function initTitles() {
